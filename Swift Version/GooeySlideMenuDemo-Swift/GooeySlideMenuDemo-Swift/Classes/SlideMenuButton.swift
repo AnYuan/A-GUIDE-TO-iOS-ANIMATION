fc15fa6fb2960f1b13c35d5c9818b0d30dd07fa8
Anyuan Dong <donganyuan1010@gmail.com>
2019-02-22 19:10:54 +0800

    [GooeySlideMenuDemo]adopt to swift 5.0

--- Swift Version/GooeySlideMenuDemo-Swift/GooeySlideMenuDemo-Swift/Classes/SlideMenuButton.swift
+++ Swift Version/GooeySlideMenuDemo-Swift/GooeySlideMenuDemo-Swift/Classes/SlideMenuButton.swift
@@ -16,50 +16,51 @@ struct MenuButtonOptions {
 
 class SlideMenuButton: UIView {
 
-    private var _option: MenuButtonOptions
+    private var option: MenuButtonOptions
     
     init(option: MenuButtonOptions) {
-        _option = option
-        super.init(frame: CGRectZero)
+        self.option = option
+        super.init(frame: CGRect.zero)
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func drawRect(rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()
-        CGContextAddRect(context, rect)
-        _option.buttonColor.set()
-        CGContextFillPath(context)
+        guard let ctx = context else { return }
         
-        let roundedRectanglePath = UIBezierPath(roundedRect: CGRectInset(rect, 1, 1), cornerRadius: rect.height / 2)
-        _option.buttonColor.setFill()
+        ctx.addRect(rect)
+        option.buttonColor.set()
+        ctx.fillPath()
+
+        let roundedRectanglePath = UIBezierPath(roundedRect: rect.insetBy(dx: 1, dy: 1), cornerRadius: rect.height / 2)
+        option.buttonColor.setFill()
         roundedRectanglePath.fill()
-        UIColor.whiteColor().setStroke()
+        UIColor.white.setStroke()
         roundedRectanglePath.lineWidth = 1
         roundedRectanglePath.stroke()
 
-        let paragraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
-        paragraphStyle.alignment = .Center
-        let attr = [NSParagraphStyleAttributeName: paragraphStyle, NSFontAttributeName: UIFont.systemFontOfSize(24.0),NSForegroundColorAttributeName: UIColor.whiteColor()]
-        let size = _option.title.sizeWithAttributes(attr)
+        let paragraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        paragraphStyle.alignment = .center
+        let attr = [NSAttributedString.Key.paragraphStyle: paragraphStyle, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24.0),NSAttributedString.Key.foregroundColor: UIColor.white]
+        let size = option.title.size(withAttributes: attr)
         
-        let r = CGRectMake(rect.origin.x,
-            rect.origin.y + (rect.size.height - size.height)/2.0,
-            rect.size.width,
-            size.height)
-        _option.title.drawInRect(r, withAttributes: attr)
-
+        let r = CGRect(x:rect.origin.x,
+                       y:rect.origin.y + (rect.size.height - size.height)/2.0,
+                       width:rect.size.width,
+                       height:size.height)
+        option.title.draw(in: r, withAttributes: attr)
     }
     
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
         if let touch = touch {
             let tapCount = touch.tapCount
             switch (tapCount) {
-                case 1: _option.buttonClickBlock()
-                default: break
+            case 1: option.buttonClickBlock()
+            default: break
             }
         }
     }
