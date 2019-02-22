8f69ede561954eb1e43bde61d41412f0f2b9b638
Anyuan Dong <donganyuan1010@gmail.com>
2019-02-22 19:10:54 +0800

    [GooeySlideMenuDemo]adopt to swift 5.0

--- Swift Version/GooeySlideMenuDemo-Swift/GooeySlideMenuDemo-Swift/Classes/GooeySlideMenu.swift
+++ Swift Version/GooeySlideMenuDemo-Swift/GooeySlideMenuDemo-Swift/Classes/GooeySlideMenu.swift
@@ -7,22 +7,22 @@
 //
 
 import UIKit
-
-typealias MenuButtonClickedBlock = (index: Int, title: String, titleCounts: Int)->()
+//index, title, titleCounts
+typealias MenuButtonClickedBlock = (Int, String, Int) -> ()
 
 struct MenuOptions {
     var titles: [String]
     var buttonHeight: CGFloat
     var menuColor: UIColor
-    var blurStyle: UIBlurEffectStyle
+    var blurStyle: UIBlurEffect.Style
     var buttonSpace: CGFloat
     var menuBlankWidth: CGFloat
     var menuClickBlock: MenuButtonClickedBlock
 }
 
-class GooeySlideMenu: UIView {
+final class GooeySlideMenu: UIView {
 
-    private var _option: MenuOptions
+    private var options: MenuOptions
     private var keyWindow: UIWindow?
     private var blurView: UIVisualEffectView!
     private var helperSideView: UIView!
@@ -34,17 +34,17 @@ class GooeySlideMenu: UIView {
     private var animationCount: Int = 0
     
     init(options: MenuOptions) {
-        _option = options
-        if let kWindow = UIApplication.sharedApplication().keyWindow{
+        self.options = options
+        if let kWindow = UIApplication.shared.keyWindow {
             keyWindow = kWindow
             let frame = CGRect(
-                x: -kWindow.frame.size.width/2 - options.menuBlankWidth,
+                x: -kWindow.frame.size.width / 2 - options.menuBlankWidth,
                 y: 0,
-                width: kWindow.frame.size.width/2 + options.menuBlankWidth,
+                width: kWindow.frame.size.width / 2 + options.menuBlankWidth,
                 height: kWindow.frame.size.height)
             super.init(frame:frame)
         } else {
-            super.init(frame:CGRectZero)
+            super.init(frame:CGRect.zero)
         }
         setUpViews()
     }
@@ -53,123 +53,137 @@ class GooeySlideMenu: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func drawRect(rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         let path = UIBezierPath()
-        path.moveToPoint(CGPoint(x: 0, y: 0))
-        path.addLineToPoint(CGPoint(x: frame.width-_option.menuBlankWidth, y: 0))
-        path.addQuadCurveToPoint(CGPoint(x: frame.width-_option.menuBlankWidth, y: frame.height), controlPoint: CGPoint(x: frame.width-_option.menuBlankWidth+diff, y: frame.height/2))
-        path.addLineToPoint(CGPoint(x: 0, y: frame.height))
-        path.closePath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: frame.width-options.menuBlankWidth, y: 0))
+        path.addQuadCurve(to: CGPoint(x: frame.width-options.menuBlankWidth, y: frame.height), controlPoint: CGPoint(x: frame.width-options.menuBlankWidth+diff, y: frame.height/2))
+        path.addLine(to: CGPoint(x: 0, y: frame.height))
+        path.close()
         
         let context = UIGraphicsGetCurrentContext()
-        CGContextAddPath(context, path.CGPath)
-        _option.menuColor.set()
-        CGContextFillPath(context)
+        context?.addPath(path.cgPath)
+        options.menuColor.set()
+        context?.fillPath()
     }
     
     func trigger() {
-        if !triggered {
-            if let keyWindow = keyWindow {
-                keyWindow.insertSubview(blurView, belowSubview: self)
-                UIView.animateWithDuration(0.3, animations: { [weak self] () -> Void in
-                    self?.frame = CGRect(
-                        x: 0,
-                        y: 0,
-                        width: keyWindow.frame.size.width/2 + _option.menuBlankWidth,
-                        height: keyWindow.frame.size.height)
-                })
-                
-                beforeAnimation()
-                UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.9, options: [.BeginFromCurrentState,.AllowUserInteraction], animations: { [weak self] () -> Void in
-                    self?.helperSideView.center = CGPointMake(keyWindow.center.x, helperSideView.frame.size.height/2);
-                    }, completion: { [weak self] (finish) -> Void in
-                        self?.finishAnimation()
-                    })
-                
-                UIView.animateWithDuration(0.3, animations: { [weak self] () -> Void in
-                    self?.blurView.alpha = 1.0
-                })
-                
-                beforeAnimation()
-                UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 2.0, options: [.BeginFromCurrentState,.AllowUserInteraction], animations: { [weak self] () -> Void in
-                    self?.helperCenterView.center = keyWindow.center
-                    }, completion: { [weak self] (finished) -> Void in
-                        if finished {
-                            let tapGesture = UITapGestureRecognizer(target: self, action: "tapToUntrigger")
-                            self?.blurView.addGestureRecognizer(tapGesture)
-                            self?.finishAnimation()
-                        }
-                })
-                animateButtons()
-                triggered = true
-            }
-        } else {
-            tapToUntrigger()
-        }
+        
+        if triggered { tapToUntrigger() }
+        
+        guard let keyWindow = keyWindow else { return }
+        
+        keyWindow.insertSubview(blurView, belowSubview: self)
+        UIView.animate(withDuration: 0.3, animations: { [weak self] () -> Void in
+            guard let nonNifSelf = self else { return }
+            nonNifSelf.frame = CGRect(
+                x: 0,
+                y: 0,
+                width: keyWindow.frame.size.width/2 + nonNifSelf.options.menuBlankWidth,
+                height: keyWindow.frame.size.height)
+        })
+        
+        beforeAnimation()
+        UIView.animate(withDuration: 0.7,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 0.9,
+                       options: [.beginFromCurrentState,.allowUserInteraction],
+                       animations: { [weak self] () -> Void in
+                        
+                        guard let nonNilSelf = self else { return }
+                        
+                        nonNilSelf.helperSideView.center = CGPoint(x: keyWindow.center.x, y: nonNilSelf.helperSideView.frame.size.height/2);
+            }, completion: { [weak self] (finish) -> Void in
+                self?.finishAnimation()
+        })
+        
+        UIView.animate(withDuration: 0.3, animations: { [weak self] () -> Void in
+            self?.blurView.alpha = 1.0
+        })
+        
+        beforeAnimation()
+        UIView.animate(withDuration: 0.7,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.8,
+                       initialSpringVelocity: 2.0,
+                       options: [.beginFromCurrentState,.allowUserInteraction],
+                       animations: { [weak self] () -> Void in
+                        
+                        self?.helperCenterView.center = keyWindow.center
+            }, completion: { [weak self] (finished) -> Void in
+                if finished {
+                    let tapGesture = UITapGestureRecognizer(target: self, action: Selector(("tapToUntrigger")))
+                    self?.blurView.addGestureRecognizer(tapGesture)
+                    self?.finishAnimation()
+                }
+        })
+        animateButtons()
+        triggered = true
     }
-    
 }
 
 extension GooeySlideMenu {
     
     private func setUpViews() {
-        if let keyWindow = keyWindow {
-            blurView = UIVisualEffectView(effect: UIBlurEffect(style: _option.blurStyle))
-            blurView.frame = keyWindow.frame
-            blurView.alpha = 0.0
-            
-            helperSideView = UIView(frame: CGRect(x: -40, y: 0, width: 40, height: 40))
-            helperSideView.backgroundColor = UIColor.redColor()
-            helperSideView.hidden = true
-            keyWindow.addSubview(helperSideView)
-            
-            helperCenterView = UIView(frame: CGRect(x: -40, y: CGRectGetHeight(keyWindow.frame)/2 - 20, width: 40, height: 40))
-            helperCenterView.backgroundColor = UIColor.yellowColor()
-            helperCenterView.hidden = true
-            keyWindow.addSubview(helperCenterView)
-            
-            backgroundColor = UIColor.clearColor()
-            keyWindow.insertSubview(self, belowSubview: helperSideView)
-            addButton()
-        }
+        
+        guard let keyWindow = keyWindow else { return }
+        
+        blurView = UIVisualEffectView(effect: UIBlurEffect(style: options.blurStyle))
+        blurView.frame = keyWindow.frame
+        blurView.alpha = 0.0
+        
+        helperSideView = UIView(frame: CGRect(x: -40, y: 0, width: 40, height: 40))
+        helperSideView.backgroundColor = .red
+        helperSideView.isHidden = true
+        keyWindow.addSubview(helperSideView)
+        
+        helperCenterView = UIView(frame: CGRect(x: -40, y: keyWindow.frame.height/2 - 20, width: 40, height: 40))
+        helperCenterView.backgroundColor = .yellow
+        helperCenterView.isHidden = true
+        keyWindow.addSubview(helperCenterView)
+        
+        backgroundColor = .clear
+        keyWindow.insertSubview(self, belowSubview: helperSideView)
+        addButton()
     }
     
     private func addButton() {
-        let titles = _option.titles
+        let titles = options.titles
         if titles.count % 2 == 0 {
             var index_down = titles.count / 2
             var index_up = -1
             for i in 0..<titles.count {
                 let title = titles[i]
-                let buttonOption = MenuButtonOptions(title:title, buttonColor:_option.menuColor, buttonClickBlock:{ [weak self] () -> () in
+                let buttonOption = MenuButtonOptions(title:title, buttonColor:options.menuColor, buttonClickBlock:{ [weak self] () -> () in
                     self?.tapToUntrigger()
-                    self?._option.menuClickBlock(index: i,title: title,titleCounts: titles.count)
+                    self?.options.menuClickBlock(i,title,titles.count)
                     })
                 let home_button = SlideMenuButton(option: buttonOption)
-                home_button.bounds = CGRectMake(0, 0, frame.width - _option.menuBlankWidth - 20*2, _option.buttonHeight);
+                home_button.bounds = CGRect(x:0, y:0, width:frame.width - options.menuBlankWidth - 20*2, height: options.buttonHeight);
                 addSubview(home_button)
                 if (i >= titles.count / 2) {
-                    index_up++
-                    let y = frame.height/2 + _option.buttonHeight*CGFloat(index_up) + _option.buttonSpace*CGFloat(index_up)
-                    home_button.center = CGPoint(x: (frame.width - _option.menuBlankWidth)/2, y: y+_option.buttonSpace/2 + _option.buttonHeight/2)
+                    index_up = index_up + 1
+                    let y = frame.height/2 + options.buttonHeight*CGFloat(index_up) + options.buttonSpace*CGFloat(index_up)
+                    home_button.center = CGPoint(x: (frame.width - options.menuBlankWidth)/2, y: y+options.buttonSpace/2 + options.buttonHeight/2)
                 } else {
-                    index_down--
-                    let y = frame.height/2 - _option.buttonHeight*CGFloat(index_down) - _option.buttonSpace*CGFloat(index_down)
-                    home_button.center = CGPoint(x: (frame.width - _option.menuBlankWidth)/2, y: y - _option.buttonSpace/2 - _option.buttonHeight/2)
+                    index_down = index_down - 1
+                    let y = frame.height/2 - options.buttonHeight*CGFloat(index_down) - options.buttonSpace*CGFloat(index_down)
+                    home_button.center = CGPoint(x: (frame.width - options.menuBlankWidth)/2, y: y - options.buttonSpace/2 - options.buttonHeight/2)
                 }
             }
         } else {
             var index = (titles.count-1) / 2 + 1
             for i in 0..<titles.count {
-                index--
+                index = index - 1
                 let title = titles[i]
-                let buttonOption = MenuButtonOptions(title: title, buttonColor: _option.menuColor, buttonClickBlock: { [weak self] () -> () in
+                let buttonOption = MenuButtonOptions(title: title, buttonColor: options.menuColor, buttonClickBlock: { [weak self] () -> () in
                     self?.tapToUntrigger()
-                    self?._option.menuClickBlock(index: i, title: title, titleCounts: titles.count)
+                    self?.options.menuClickBlock(i, title, titles.count)
                     })
                 let home_button = SlideMenuButton(option: buttonOption)
-                home_button.bounds = CGRect(x: 0, y: 0, width: frame.width - _option.menuBlankWidth - 20*2, height: _option.buttonHeight)
-                home_button.center = CGPoint(x: (frame.width - _option.menuBlankWidth)/2, y: frame.height/2 - _option.buttonHeight*CGFloat(index) - 20*CGFloat(index))
+                home_button.bounds = CGRect(x: 0, y: 0, width: frame.width - options.menuBlankWidth - 20*2, height: options.buttonHeight)
+                home_button.center = CGPoint(x: (frame.width - options.menuBlankWidth)/2, y: frame.height/2 - options.buttonHeight*CGFloat(index) - 20*CGFloat(index))
                 addSubview(home_button)
             }
         }
@@ -178,36 +192,43 @@ extension GooeySlideMenu {
     private func animateButtons() {
         for i in 0..<subviews.count {
             let menuButton = subviews[i]
-            menuButton.transform = CGAffineTransformMakeTranslation(-90, 0)
-            UIView.animateWithDuration(0.7, delay: Double(i)*(0.3/Double(subviews.count)), usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: [.BeginFromCurrentState,.AllowUserInteraction], animations: { () -> Void in
-                    menuButton.transform =  CGAffineTransformIdentity
+            menuButton.transform = CGAffineTransform(translationX: -90, y: 0)
+            UIView.animate(withDuration: 0.7, delay: Double(i)*(0.3/Double(subviews.count)), usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: [.beginFromCurrentState,.allowUserInteraction], animations: { () -> Void in
+                    menuButton.transform =  .identity
                 }, completion: nil)
         }
     }
     
    @objc private func tapToUntrigger() {
-        UIView.animateWithDuration(0.3) { [weak self] () -> Void in
-            self?.frame = CGRect(
-                x: -keyWindow!.frame.size.width/2 - _option.menuBlankWidth,
-                y: 0,
-                width: keyWindow!.frame.size.width/2 + _option.menuBlankWidth,
-                height: keyWindow!.frame.size.height)
-        }
     
-        beforeAnimation()
-        UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.9, options: [.BeginFromCurrentState,.AllowUserInteraction], animations: { () -> Void in
+    UIView.animate(withDuration: 0.3) { [weak self] () -> Void in
+        
+    guard let nonNilSelf = self, let keyWindow = self?.keyWindow else { return }
+        
+    let x = -keyWindow.frame.size.width/2 - nonNilSelf.options.menuBlankWidth
+    let width = keyWindow.frame.size.width/2 + nonNilSelf.options.menuBlankWidth
+    nonNilSelf.frame = CGRect(
+        x: x,
+        y: 0,
+        width: width,
+        height: keyWindow.frame.size.height)
+    }
+    
+    beforeAnimation()
+    
+    UIView.animate(withDuration: 0.7, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.9, options: [.beginFromCurrentState,.allowUserInteraction], animations: { () -> Void in
             self.helperSideView.center = CGPoint(x: -self.helperSideView.frame.height/2, y: self.helperSideView.frame.height/2)
             }) { [weak self] (finish) -> Void in
                 self?.finishAnimation()
         }
         
-        UIView.animateWithDuration(0.3) { () -> Void in
+    UIView.animate(withDuration: 0.3) { () -> Void in
             self.blurView.alpha = 0.0
         }
         
         beforeAnimation()
-        UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 2.0, options: [.BeginFromCurrentState,.AllowUserInteraction], animations: { () -> Void in
-            self.helperCenterView.center = CGPointMake(-self.helperSideView.frame.size.height/2, CGRectGetHeight(self.frame)/2)
+    UIView.animate(withDuration: 0.7, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 2.0, options: [.beginFromCurrentState,.allowUserInteraction], animations: { () -> Void in
+        self.helperCenterView.center = CGPoint(x:-self.helperSideView.frame.size.height/2, y:self.frame.size.height/2)
             }) { (finish) -> Void in
                 self.finishAnimation()
         }
@@ -216,28 +237,29 @@ extension GooeySlideMenu {
     
     private func beforeAnimation() {
         if displayLink == nil {
-            displayLink = CADisplayLink(target: self, selector: "handleDisplayLinkAction:")
-            displayLink?.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+            displayLink = CADisplayLink(target: self, selector: Selector(("handleDisplayLinkAction:")))
+            displayLink?.add(to: RunLoop.main, forMode: RunLoop.Mode.default)
         }
-        animationCount++
+        animationCount = animationCount + 1
     }
     
     private func finishAnimation() {
-        animationCount--
+        animationCount = animationCount - 1
         if animationCount == 0 {
             displayLink?.invalidate()
             displayLink = nil
         }
     }
     
-    @objc private func handleDisplayLinkAction(displaylink: CADisplayLink) {
-        let sideHelperPresentationLayer = helperSideView.layer.presentationLayer() as! CALayer
-        let centerHelperPresentationLayer = helperCenterView.layer.presentationLayer() as! CALayer
+    @objc private func handleDisplayLinkAction(_ displaylink: CADisplayLink) {
         
-        let centerRect = centerHelperPresentationLayer.valueForKeyPath("frame")?.CGRectValue
-        let sideRect   = sideHelperPresentationLayer.valueForKeyPath("frame")?.CGRectValue
+        guard let sideHelperPresentationLayer = helperSideView.layer.presentation(),
+            let centerHelperPresentationLayer = helperCenterView.layer.presentation() else { return }
         
-        if let centerRect = centerRect, sideRect = sideRect {
+        let centerRect = (centerHelperPresentationLayer.value(forKeyPath: "frame") as AnyObject).cgRectValue
+        let sideRect   = (sideHelperPresentationLayer.value(forKeyPath: "frame") as AnyObject).cgRectValue
+        
+        if let centerRect = centerRect, let sideRect = sideRect {
             diff = sideRect.origin.x - centerRect.origin.x
         }
         setNeedsDisplay()
